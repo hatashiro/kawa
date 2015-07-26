28d85232caa86bddba9ce7d40b51b90a47046caf
Hyunje Alex Jun <me@noraesae.net>
2015-07-27 01:22:34 +0900

    Add basic Xcode project files.

---
+++ kawa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  kawa
+//
+//  Created by noraesae on 27/07/2015.
+//  Copyright (c) 2015 noraesae. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ kawa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  kawa
+//
+//  Created by noraesae on 27/07/2015.
+//  Copyright (c) 2015 noraesae. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
