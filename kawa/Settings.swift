6fcb325e9fbe7c5bfd5afa09efe2beec80bb84fe
Hyunje Alex Jun <me@noraesae.net>
2015-08-02 02:39:52 +0900

    Add settings and bind them with SystemSettingViewController.

---
+++ kawa/Settings.swift
@@ -0,0 +1,30 @@
+//
+//  Settings.swift
+//  kawa
+//
+//  Created by noraesae on 02/08/2015.
+//  Copyright (c) 2015 noraesae. All rights reserved.
+//
+
+import Cocoa
+
+class Settings {
+    static let defaults = NSUserDefaults.standardUserDefaults()
+
+    static let showMenubarIcon = "show-menubar-icon"
+    static let launchOnStartup = "launch-on-startup"
+
+    static func get<T>(key: String, withDefaultValue: T) -> T {
+        let val: T? = defaults.objectForKey(key) as? T
+        if val != nil {
+            return val!
+        } else {
+            return withDefaultValue
+        }
+    }
+
+    static func set<T>(key: String, toValue: T) {
+        defaults.setObject((toValue as! AnyObject), forKey: key)
+        defaults.synchronize()
+    }
+}
--- kawa/SystemSettingViewController.swift
+++ kawa/SystemSettingViewController.swift
@@ -9,6 +9,8 @@
 import Cocoa
 
 class SystemSettingViewController: NSViewController {
+    @IBOutlet var showMenubarIconCheckbox: NSButton!
+    @IBOutlet var launchOnStartupCheckbox: NSButton!
     @IBOutlet var quitAppButton: NSButton!
 
     override func viewDidLoad() {
@@ -16,9 +18,29 @@ class SystemSettingViewController: NSViewController {
 
         quitAppButton.target = self
         quitAppButton.action = Selector("quitApp:")
+
+        showMenubarIconCheckbox.target = self
+        showMenubarIconCheckbox.action = Selector("setShowMenubarIcon:")
+        var isOn: Bool = Settings.get(Settings.showMenubarIcon, withDefaultValue: true)
+        showMenubarIconCheckbox.state = isOn ? NSOnState : NSOffState
+
+        launchOnStartupCheckbox.target = self
+        launchOnStartupCheckbox.action = Selector("setLaunchOnStartup:")
+        isOn = Settings.get(Settings.launchOnStartup, withDefaultValue: true)
+        launchOnStartupCheckbox.state = isOn ? NSOnState: NSOffState
     }
 
     func quitApp(sender: AnyObject) {
         NSApplication.sharedApplication().terminate(nil)
+    }
+
+    func setShowMenubarIcon(sender: AnyObject) {
+        let isOn: Bool = showMenubarIconCheckbox.state == NSOnState
+        Settings.set(Settings.showMenubarIcon, toValue: isOn)
+    }
+
+    func setLaunchOnStartup(sender: AnyObject) {
+        let isOn: Bool = launchOnStartupCheckbox.state == NSOnState
+        Settings.set(Settings.launchOnStartup, toValue: isOn)
     }
 }
