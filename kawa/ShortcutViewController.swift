02540f532ab8227b0df2872ae04d3e8f97d4eb89
Hyunjae Jun <noraesae+dev@gmail.com>
2020-07-09 04:16:29 +0900

    Simplify table view.

--- kawa/ShortcutViewController.swift
+++ kawa/ShortcutViewController.swift
@@ -1,16 +1,33 @@
 import Cocoa
 
-class ShortcutViewController: NSViewController {
-  @IBOutlet weak var tableView: ShortcutTableView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    loadInputSources()
+class ShortcutViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return InputSource.sources.count
   }
 
-  func loadInputSources() {
-    tableView.dataSource = tableView
-    tableView.delegate = tableView
-    tableView.reloadData()
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    let inputSource = InputSource.sources[row]
+
+    switch tableColumn!.identifier.rawValue {
+    case "Keyboard":
+      return createKeyboardCellView(tableView, inputSource)
+    case "Shortcut":
+      return createShorcutCellView(tableView, inputSource)
+    default:
+      return nil
+    }
+  }
+
+  func createKeyboardCellView(_ tableView: NSTableView, _ inputSource: InputSource) -> NSTableCellView? {
+    let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "KeyboardCellView"), owner: self) as? NSTableCellView
+    cell!.textField?.stringValue = inputSource.name
+    cell!.imageView?.image = inputSource.icon
+    return cell
+  }
+
+  func createShorcutCellView(_ tableView: NSTableView, _ inputSource: InputSource) -> ShortcutCellView? {
+    let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "ShortcutCellView"), owner: self) as? ShortcutCellView
+    cell?.setInputSource(inputSource)
+    return cell
   }
 }
