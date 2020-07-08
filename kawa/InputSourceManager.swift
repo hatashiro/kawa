c1491a0c789a547b157ab104ddcfa909ae897ceb
Hyunjae Jun <noraesae+dev@gmail.com>
2020-07-09 02:38:19 +0900

    Remove CJK-specific logic.

--- kawa/InputSourceManager.swift
+++ kawa/InputSourceManager.swift
@@ -17,13 +17,6 @@ class InputSource: Equatable {
         return tisInputSource.name
     }
 
-    var isCJKV: Bool {
-        if let lang = tisInputSource.sourceLanguages.first {
-            return lang == "ko" || lang == "ja" || lang == "vi" || lang.hasPrefix("zh")
-        }
-        return false
-    }
-
     init(tisInputSource: TISInputSource) {
         self.tisInputSource = tisInputSource
 
@@ -47,15 +40,6 @@ class InputSource: Equatable {
 
     func select() {
         TISSelectInputSource(tisInputSource)
-
-        if isCJKV, let selectPreviousShortcut = InputSourceManager.getSelectPreviousShortcut() {
-            // Workaround for TIS CJKV layout bug:
-            // when it's CJKV, select nonCJKV input first and then return
-            if let nonCJKV = InputSourceManager.nonCJKVSource() {
-                nonCJKV.select()
-                InputSourceManager.selectPrevious(shortcut: selectPreviousShortcut)
-            }
-        }
     }
 }
 
@@ -69,53 +53,6 @@ class InputSourceManager {
         inputSources = inputSourceList.filter({
             $0.category == TISInputSource.Category.keyboardInputSource && $0.isSelectable
         }).map { InputSource(tisInputSource: $0) }
-    }
-    
-    static func nonCJKVSource() -> InputSource? {
-        return inputSources.first(where: { !$0.isCJKV })
-    }
-
-    static func selectPrevious(shortcut: (Int, UInt64)) {
-        let src = CGEventSource(stateID: .hidSystemState)
-
-        let key = CGKeyCode(shortcut.0)
-        let flag = CGEventFlags(rawValue: shortcut.1)
-
-        let down = CGEvent(keyboardEventSource: src, virtualKey: key, keyDown: true)!
-        let up = CGEvent(keyboardEventSource: src, virtualKey: key, keyDown: false)!
-
-        down.flags = flag;
-        up.flags = flag;
-
-        down.post(tap: .cghidEventTap)
-        up.post(tap: .cghidEventTap)
-    }
-
-    // from read-symbolichotkeys script of Karabiner
-    // github.com/tekezo/Karabiner/blob/master/src/util/read-symbolichotkeys/read-symbolichotkeys/main.m
-    static func getSelectPreviousShortcut() -> (Int, UInt64)? {
-        guard let dict = UserDefaults.standard.persistentDomain(forName: "com.apple.symbolichotkeys") else {
-            return nil
-        }
-        guard let symbolichotkeys = dict["AppleSymbolicHotKeys"] as! NSDictionary? else {
-            return nil
-        }
-        guard let symbolichotkey = symbolichotkeys["60"] as! NSDictionary? else {
-            return nil
-        }
-        if (symbolichotkey["enabled"] as! NSNumber).intValue != 1 {
-            return nil
-        }
-        guard let value = symbolichotkey["value"] as! NSDictionary? else {
-            return nil
-        }
-        guard let parameters = value["parameters"] as! NSArray? else {
-            return nil
-        }
-        return (
-            (parameters[1] as! NSNumber).intValue,
-            (parameters[2] as! NSNumber).uint64Value
-        )
     }
 }
 
