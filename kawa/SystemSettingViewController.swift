f6cbb4542cb23952ee4ca745b267fe1a34e3e8a1
Hyunjae Jun <noraesae+dev@gmail.com>
2020-07-09 03:37:38 +0900

    Make quitApp an IBAction.

--- kawa/SystemSettingViewController.swift
+++ kawa/SystemSettingViewController.swift
@@ -2,18 +2,14 @@ import Cocoa
 
 class SystemSettingViewController: NSViewController {
   @IBOutlet weak var showNotificationCheckbox: NSButton!
-  @IBOutlet var quitAppButton: NSButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    quitAppButton.target = self
-    quitAppButton.action = #selector(SystemSettingViewController.quitApp(_:))
-
     showNotificationCheckbox.state = Storage.showsNotification.stateValue
   }
 
-  @objc func quitApp(_ sender: AnyObject) {
+  @IBAction func quitApp(_ sender: NSButton) {
     NSApplication.shared.terminate(nil)
   }
 
